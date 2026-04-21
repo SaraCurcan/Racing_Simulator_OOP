5337b2bddbfab798f17f2bda9f68d70e0fecf4c2
Curcan Sara <saracurcan06@gmail.com>
2026-04-21 16:17:41 +0300

    added some exceptions in exception class

--- Exceptions.h
+++ Exceptions.h
@@ -1,8 +1,23 @@
-//
-// Created by sara on 4/21/26.
-//
+#pragma once
+#include<string>
+#include<exception>
+using namespace std;
 
-#ifndef VEHICLESIMULATOR_EXCEPTIONS_H
-#define VEHICLESIMULATOR_EXCEPTIONS_H
+class InvalidNameException:public exception {
+    string message;
+public:
+    InvalidNameException(const string& message="This field cannot be empty!"):message(message){}
 
-#endif //VEHICLESIMULATOR_EXCEPTIONS_H
+    const char* what() const noexcept override {
+        return message.c_str();
+    }
+};
+
+class InvalidNumber: public exception {
+    string message;
+public:
+    InvalidNumber(const string& message="Enter a positive number"):message(message){}
+    const char* what() const noexcept override {
+        return message.c_str();
+    }
+};
