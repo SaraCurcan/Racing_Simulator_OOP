dd17f8d17dab4d2337ee3cbbca94b981be4fe7c4
Curcan Sara <saracurcan06@gmail.com>
2026-04-21 17:09:41 +0300

    Added vehicle class methods

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -2,24 +2,24 @@
 #include "Exceptions.h"
 #include<iostream>
 #include<string>
-Vehicle::Vehicle():name(""),brand(""),speed(0.0),price(0.0),maxSpeed(0.0){}
-Vehicle::Vehicle(std::string name, std::string brand, double maxSpeed, double price, double speed) {
-    if (name.empty() || brand.empty()) {
+Vehicle::Vehicle():type(""),brand(""),speed(0.0),price(0.0),maxSpeed(0.0){}
+Vehicle::Vehicle(std::string type, std::string brand, double maxSpeed, double price, double speed) {
+    if (type.empty() || brand.empty()) {
         throw InvalidNameException();
     }
     if (maxSpeed<=0 || speed<0 || price<0) {
         throw InvalidNumber();
     }
-    this->name=name;
+    this->type=type;
     this->brand=brand;
     this->maxSpeed=maxSpeed;
     this->speed=speed;
     this->price=price;
 }
-Vehicle::Vehicle(const Vehicle& obj):name(obj.name), brand(obj.brand),maxSpeed(obj.maxSpeed),speed(obj.speed),price(obj.price){}
+Vehicle::Vehicle(const Vehicle& obj):type(obj.type), brand(obj.brand),maxSpeed(obj.maxSpeed),speed(obj.speed),price(obj.price){}
 Vehicle& Vehicle::operator=(const Vehicle& obj) {
     if (this!=&obj) {
-        this->name=obj.name;
+        this->type=obj.type;
         this->brand=obj.brand;
         this->maxSpeed=obj.maxSpeed;
         this->speed=obj.speed;
@@ -28,3 +28,35 @@ Vehicle& Vehicle::operator=(const Vehicle& obj) {
     return *this;
 }
 Vehicle::~Vehicle() {}
+
+void Vehicle::pressBreak() {
+    speed=0;
+}
+
+void Vehicle::upgrade() {
+    maxSpeed+=15;
+}
+
+std::string Vehicle::getType() const {return type;}
+std::string Vehicle::getBrand() const {return brand;}
+double Vehicle::getMaxSpeed() const {return maxSpeed;}
+
+std::ostream& operator<<(std::ostream& out, const Vehicle& obj) {
+    out<<"Brand: "<<obj.getBrand()<<std::endl;
+    out<<"Type: "<<obj.getType()<<std::endl;
+    out<<"Max Speed: "<<obj.getMaxSpeed()<<std::endl;
+    return out;
+}
+std::istream& operator>>(std::istream& in , Vehicle& obj) {
+    std::cout<<"Brand: ";
+    getline(in,obj.brand);
+    if (obj.brand.empty()) throw InvalidNameException();
+    std::cout<<"Type: ";
+    getline(in,obj.type);
+    if (obj.type.empty()) throw InvalidNameException();
+    std::cout<<"Max Speed: ";
+    in>>obj.maxSpeed;
+    if (obj.maxSpeed<=0) throw InvalidNumber();
+    in.ignore();
+    return in;
+}
