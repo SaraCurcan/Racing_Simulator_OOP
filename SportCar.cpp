eb4ded9835fc9d4cdf1bf17cdb962d4dedc3333b
Curcan Sara <saracurcan06@gmail.com>
2026-04-21 20:02:59 +0300

    Added SportCar constructors, destructor and operatro=

--- SportCar.cpp
+++ SportCar.cpp
@@ -1,3 +1,17 @@
-//
-// Created by sara on 4/21/26.
-//
+#include "SportCar.h"
+#include "Exceptions.h"
+#include<iostream>
+#include<string>
+SportCar::SportCar():Vehicle(), availableNitro(0){}
+SportCar::SportCar(std::string type, std::string brand, double maxSpeed, double price, double speed, int availableNitro)
+    :Vehicle(type,brand,maxSpeed,price,speed,1),availableNitro(availableNitro){}
+
+SportCar::SportCar(const SportCar &obj):Vehicle(obj), availableNitro(obj.availableNitro){}
+SportCar& SportCar::operator=(const SportCar& obj) {
+    if (this!=&obj) {
+        Vehicle::operator=(obj);
+        availableNitro=obj.getAvailableNitro();
+    }
+    return *this;
+}
+SportCar::~SportCar(){}
